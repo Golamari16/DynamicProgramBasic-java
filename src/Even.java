71a04b8b351b9b2e1f07bcdac59190dcd35090f7
Golamari16 <mahithareddygolamari@gmail.com>
2026-05-16 22:57:45 +0530

    even program added

---
+++ src/Even.java
@@ -0,0 +1,16 @@
+// check the given input is even or odd
+
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        if(n%2==0)
+            System.out.println("it is even number");
+        else
+            System.out.println("it is odd number");
+        sc.close();
+    }
+}
